3495868c5b9062453a33963e691e819d06f64e45
Simon Karapetyan <simon.karapetyan@esterox.am>
2023-05-15 09:43:50 +0400

    Added new function

--- main.py
+++ main.py
@@ -1,72 +1,72 @@
-# x = 10
-# y = 20
-# print(x + y)
+x = 10
+y = 20
+print(x + y)
 
 
 
-# fruits = ["apple", "banana", "cherry"]
-# for fruit in fruits:
-#     print(fruit)
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
 
 
 
-# i = 0
-# while i < 10:
-#     print(i)
-#     i += 1
+i = 0
+while i < 10:
+    print(i)
+    i += 1
 
 
-# def print_arguments(*args, **kwargs):
-#     print("Positional arguments:")
-#     for arg in args:
-#         print(arg)
-#
-#     print("Keyword arguments:")
-#     for key, value in kwargs.items():
-#         print(key, ":", value)
-#
-# print_arguments("apple", "banana", "cherry", color="red", taste="sweet")
+def print_arguments(*args, **kwargs):
+    print("Positional arguments:")
+    for arg in args:
+        print(arg)
+
+    print("Keyword arguments:")
+    for key, value in kwargs.items():
+        print(key, ":", value)
+
+print_arguments("apple", "banana", "cherry", color="red", taste="sweet")
 
 
-# class Rectangle:
-#     def __init__(self, width, height):
-#         self.width = width
-#         self.height = height
-#
-#     def area(self):
-#         return self.width * self.height
-#
-#     def perimeter(self):
-#         return 2 * (self.width + self.height)
-#
-# rect = Rectangle(5, 3)
-# area = rect.area()
-# perimeter = rect.perimeter()
-#
-# print(area) # 15
-# print(perimeter) # 16
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+rect = Rectangle(5, 3)
+area = rect.area()
+perimeter = rect.perimeter()
+
+print(area) # 15
+print(perimeter) # 16
 
 
-# Inheritance
-# class Animal:
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def speak(self):
-#         print("The animal makes a sound.")
-#
-#
-# class Dog(Animal):
-#     def __init__(self, name, breed):
-#         super().__init__(name)
-#         self.breed = breed
-#
-#     def speak(self):
-#         print("The dog barks: Woof!")
-#
-#     def fetch(self):
-#         print("The dog fetches a ball.")
+Inheritance
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print("The animal makes a sound.")
 
 
-# from functions import get_date
-# print(get_date())
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def speak(self):
+        print("The dog barks: Woof!")
+
+    def fetch(self):
+        print("The dog fetches a ball.")
+
+
+from functions import get_date
+print(get_date())
