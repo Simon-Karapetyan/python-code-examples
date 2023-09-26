9c3d68462cb763f1acfbc0149fc5a895dc03df3c
Simon Karapetyan <simon.karapetyan@esterox.am>
2023-09-26 08:56:15 +0400

    Created turtle graphics

---
+++ turtle-graphics/rectangle.py
@@ -0,0 +1,14 @@
+import turtle
+
+rect = turtle.Turtle()
+
+rect.forward(100)
+rect.right(90)
+rect.forward(100)
+rect.right(90)
+rect.forward(100)
+rect.right(90)
+rect.forward(100)
+rect.right(90)
+
+turtle.done()
